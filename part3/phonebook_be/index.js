bbf91606a36232b9f5328a97e93cc6aff297b5cd
Allison Tielking <atielkin@alumni.stanford.edu>
2022-12-29 21:56:55 -0600

    3.13 and 3.14

--- part3/phonebook_be/index.js
+++ part3/phonebook_be/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
 const app = express()
+const Contact = require('./models/contact')
 
 app.use(express.json())
 app.use(cors())
@@ -35,19 +37,17 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+  Contact.findById(request.params.id).then(contact => {
+    response.json(contact)
   })
+})
+
   app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
@@ -55,47 +55,29 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(204).end()
   })
 
-  const generateId = () => {
-    const maxId = Math.floor(Math.random() * 100000)
-    return maxId
-  }
-
   app.post('/api/persons', (request, response) => {
     const body = request.body
   
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
+    if (body.name === undefined) {
+      return response.status(400).json({ error: 'name missing' })
     }
-    const personSearch = persons.find(person => person.name === body.name)
-    if (personSearch) {
-        return response.status(409).json({
-            error: 'name must be unique'
-        })
-    }
-    if (!body.number) {
-        return response.status(400).json({ 
-          error: 'number missing' 
-        })
-      }
   
-    const person = {
-      number: body.number,
+    const contact = new Contact({
       name: body.name,
-      id: generateId(),
-    }
+      number: body.number,
+      date: new Date(),
+    })
   
-    persons = persons.concat(person)
-  
-    response.json(person)
+    contact.save().then(savedContact => {
+      response.json(savedContact)
+    })
   })
 
 app.get('/api/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
