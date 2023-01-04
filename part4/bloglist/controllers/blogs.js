0bbc47ce0e9666b2b2440a4d58dfb18b9b502597
Allison Tielking <atielkin@alumni.stanford.edu>
2023-01-04 16:09:19 -0600

    move token extractor to middleware

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.get('/', async(request, response) => {
   const blogs = await Blog.find({})
     .populate('user', { username: 1, name: 1 })
@@ -19,10 +11,7 @@ blogsRouter.get('/', async(request, response) => {
 
 blogsRouter.post('/', async(request, response) => {
   const body = request.body
-  console.log(body)
-  const token = getTokenFrom(request)
-  console.log(token)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
@@ -35,7 +24,6 @@ blogsRouter.post('/', async(request, response) => {
     })
   }
   const user = await User.findById(body.userId)
-  console.log(user)
   const blog = new Blog({
     title: body.title,
     author: body.author,
