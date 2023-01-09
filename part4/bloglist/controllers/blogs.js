af8e9bdffc7ea1291ee960078a8762575b2c7a9c
Allison Tielking <atielkin@alumni.stanford.edu>
2023-01-09 15:50:11 -0600

    fixed bugs backend

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -11,7 +11,6 @@ blogsRouter.get('/', async(request, response) => {
 
 blogsRouter.post('/', async(request, response) => {
   const body = request.body
-  console.log(body)
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
@@ -24,14 +23,13 @@ blogsRouter.post('/', async(request, response) => {
       error: 'blog title must be unique'
     })
   }
-  console.log(request.user)
   const user = request.user
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: user.id
+    user: user
   })
 
   const savedBlog = await blog.save()
@@ -61,11 +59,14 @@ blogsRouter.delete('/:id', async(request, response) => {
   const blog = await Blog.findById(request.params.id)
   if (blog) {
     if (blog.user.toString() !== user.id.toString()) {
-      return response.status(401).json({ error: 'cannot delete another user blog' })
+      return response.status(401).json({ error: 'cannot delete another user\'s blog' })
     }
   } else {
-    response.status(404).end()
+    return response.status(404).end()
   }
+  const userObject = await User.findById(user.id.toString())
+  const userObjectBlogs = userObject.blogs
+  await User.findByIdAndUpdate(blog.user.toString(), { blogs: userObjectBlogs.filter(blog => blog.id.toString() !== request.params.id) })
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
@@ -77,7 +78,8 @@ blogsRouter.put('/:id', async(request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: body.user
   }
   const blogResult = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true,
     runValidators: true, context: 'query' })
