f8f76ec6087789f4834afee51daf6829c78ce2fa
Allison Tielking <atielkin@alumni.stanford.edu>
2023-01-10 11:49:42 -0600

    bloglist backend cypress

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken')
 blogsRouter.get('/', async(request, response) => {
   const blogs = await Blog.find({})
     .populate('user', { username: 1, name: 1 })
+  console.log(blogs)
   response.json(blogs)
 })
 
