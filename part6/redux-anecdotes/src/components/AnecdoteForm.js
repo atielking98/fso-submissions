f647e101a284b5d5f0223378a8790867223c17b3
Allison Tielking <atielkin@alumni.stanford.edu>
2023-01-12 21:57:59 -0600

    finish part 6a

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+
+  const dispatch = useDispatch()
+
+  const create = (event) => {
+    console.log('create anecdote')
+    event.preventDefault()
+    const anecdote = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(anecdote))
+  }
+    return (
+        <div>
+            <h2>create new</h2>
+            <form onSubmit={create}>
+                <div><input name="anecdote"/></div>
+                <button type="submit">create</button>
+            </form>
+        </div>
+    )
+    
+}
+
+export default AnecdoteForm
