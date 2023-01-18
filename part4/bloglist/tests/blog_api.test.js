b00ada9d6003149554f2c73c8cb120e3169da195
Allison Tielking <atielkin@alumni.stanford.edu>
2023-01-18 11:08:35 -0600

    nice

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -4,7 +4,7 @@ const app = require('../app')
 
 const api = supertest(app)
 const helper = require('./test_helper')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 const Blog = require('../models/blog')
 const User = require('../models/user')
